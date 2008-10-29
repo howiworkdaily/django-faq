41d5200bec73b938f7d5d4a274baa0b1d3ba0e71
brosner <brosner@gmail.com>
2008-10-29 17:49:39 -0600

    Revert "First stab at per-object questions. It still needs a bit more work."
    This reverts commit 786283f71620f3cbc7983e0fd6a866ed68bb5a3b.
    
    Whoops, wrong branch :)

--- faq/managers.py
+++ faq/managers.py
@@ -1,11 +1,8 @@
-
-import datetime
-
 from django.db import models
 from django.db.models.query import QuerySet
-from django.contrib.contenttypes.models import ContentType
-
+import datetime
 import faq.enums as enums
+
 
 class QuestionQuerySet(QuerySet):
     """
@@ -32,16 +29,12 @@ class QuestionManager(models.Manager):
     to helpful utility methods.
 
     """
-    
+
     def get_query_set(self):
         return QuestionQuerySet(self.model)
-        
+
     def active(self, slug=None):
         return self.get_query_set().active(slug=slug)
-    
-    def get_for_object(self, obj):
-        """
-        Creates a queryset matching all questions for the given object.
-        """
-        ct = ContentType.objects.get_for_model(obj)
-        return self.filter(content_type=ct, object_pk=obj.pk)
+
+
+
