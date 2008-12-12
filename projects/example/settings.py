5103a3f9e904b2c0ee327ce6cf0b3894a046d42a
rock <rock>
2008-12-12 16:13:18 +0000

    Reorg for multiple projects.

--- projects/example/settings.py
+++ projects/example/settings.py
@@ -77,7 +77,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	[os.path.join(PROJECT_DIR, "templates")]
+    [os.path.join(PROJECT_DIR, "templates")]
 )
 
 INSTALLED_APPS = (
@@ -85,8 +85,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-	'django.contrib.admin',
-	'faq',
+    'django.contrib.admin',
+    'faq',
 )
 
 
