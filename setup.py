91e134a0b22a281df4f8274bf1575d05f99f41be
Rock Howard <rockmhoward@gmail.com>
2009-01-28 21:54:39 -0600

    added setup.py for Pinax installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-faq',
+    version='0.1.0',
+    description='This is a simple FAQ application.',
+    author='Kevin Fricovsky',
+    author_email='kfricovsky@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
