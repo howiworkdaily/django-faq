f1d1d9ae23806c4b1592c9041f0576eec9d5f59e
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-05-09 14:46:07 -0500

    Updated template tags to be more modern:
    * Use token.split_contents instead of token.contents.split.
    * Use variables where apropriate to allow for more dynamic uses.
    
    This is a backwards-incompatible change.

--- faq/templatetags/faqtags.py
+++ faq/templatetags/faqtags.py
@@ -1,67 +1,80 @@
+from __future__ import absolute_import
+
 from django import template
-from django.template import Library, Node, Variable, TemplateSyntaxError
-from faq.enums import *
+from .enums import STATUS_ACTIVE
+from .models import Question, Topic
 
-from faq.models import Question
+register = template.Library()
 
-register = Library()
-
-
-class FaqListNode(Node):
+class FaqListNode(template.Node):
     def __init__(self, num, topic, varname):
-        self.num, self.topic, self.varname = num, topic, varname
+        self.num = template.Variable(num)
+        self.topic = template.Variable(topic)
+        self.varname = varname
 
     def render(self, context):
-        context[self.varname] = Question.objects.filter(
-            topic__slug=self.topic,
-            status__exact=STATUS_ACTIVE)[:self.num]
+        try:
+            num = self.num.resolve(context)
+            topic = self.topic.resolve(context)
+        except template.VariableDoesNotExist:
+            return ''
+            
+        if isinstance(topic, Topic):
+            qs = Question.objects.filter(topic=topic)
+        else:
+            qs = Question.objects.filter(topic__slug=topic)
+        
+        context[self.varname] = qs.filter(status=STATUS_ACTIVE)[:num]
+
         return ''
 
-
-def do_faqs_for_topic(parser, token):
+@register.tag
+def faqs_for_topic(parser, token):
     """
-    returns a list of 'count' faq's that belong to the given topic
+    Returns a list of 'count' faq's that belong to the given topic
     the supplied topic argument must be in the slug format 'topic-name'
     
-    Example usage:
-    {% faqs_for_topic [5] [topicslug] as [faqs] %}
+    Example usage::
+    
+        {% faqs_for_topic 5 "my-slug" as faqs %}
     """
 
-    args = token.contents.split()
+    args = token.split_contents()
     if len(args) != 5:
-        raise TemplateSyntaxError, "faqs_for_topic tag takes exactly four arguments"
+        raise template.TemplateSyntaxError("%s takes exactly four arguments" % args[0])
     if args[3] != 'as':
-        raise TemplateSyntaxError, "third argument to the faqs_for_topic tag must be 'as'"
+        raise template.TemplateSyntaxError("third argument to the %s tag must be 'as'" % args[0])
 
     return FaqListNode(args[1], args[2], args[4])
-    
-register.tag('faqs_for_topic', do_faqs_for_topic)
 
-
-class FaqNode(Node):
+class FaqNode(template.Node):
     def __init__(self, num, varname):
-        self.num, self.varname = num, varname
+        self.num = template.Variable(num)
+        self.varname = varname
 
     def render(self, context):
-        context[self.varname] = Question.objects.filter(
-            status__exact=STATUS_ACTIVE)[:self.num]
+        try:
+            num = self.num.resolve(context)
+        except template.VariableDoesNotExist:
+            return ''
+            
+        context[self.varname] = Question.objects.filter(status=STATUS_ACTIVE)[:num]
         return ''
 
+@register.tag
 def do_faq_list(parser, token):
     """
     returns a generic list of 'count' faq's to display in a list 
     ordered by the faq sort order.
 
-    Example usage:
-    {% faq_list [5] as [faqs] %}
+    Example usage::
+    
+        {% faq_list 15 as faqs %}
     """
-
-    args = token.contents.split()
+    args = token.split_contents()
     if len(args) != 4:
-        raise TemplateSyntaxError, "faq_list tag takes exactly four arguments"
+        raise template.TemplateSyntaxError("%s takes exactly three arguments" % args[0])
     if args[2] != 'as':
-        raise TemplateSyntaxError, "second argument to the faq_list tag must be 'as'"
+        raise template.TemplateSyntaxError("second argument to the %s tag must be 'as'" % args[0])
 
     return FaqNode(args[1], args[3])
-
-register.tag('faq_list', do_faq_list)
